e655ca0ed3ecc9034cc3a95d68afc6c2e1b79b06
Hemant Pillai <hemant.pillai8@gmail.com>
2025-04-03 00:03:38 +0530

    Notification Module Designed

---
+++ src/components/CustomRenderItem/CustNotificationItem.jsx
@@ -0,0 +1,334 @@
+import { Dimensions, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React from 'react';
+import { RFValue } from 'react-native-responsive-fontsize';
+import Colors from '../../constants/Colors';
+
+
+import LocationIcon from '../../assets/images/ic_Location.svg';
+import TimeIcon from '../../assets/images/ic_Time.svg';
+import AssuredPayIcon from '../../assets/images/ic_BdgPaymentAssured.svg';
+import VerifiedIcon from '../../assets/images/ic_BdgVerified.svg';
+import RatingIcon from '../../assets/images/ic_BdgRating.svg';
+import Constants from '../../constants/Constants';
+
+import moment from 'moment';
+
+
+const CustNotificationItem = ({ item, }) => {
+
+
+    const ColorType = {
+        inprocess: '#FF8B00',
+        requested: '#FF8B00',
+        ordered: '#FF8B00',
+        completed: '#89D373',
+        declined: '#FF4D68',
+        cancelled: '#A3A3A3',
+        accepted: '#408BFC',
+    };
+
+    const StatusText = {
+        inprocess: 'Your order has been placed',
+        requested: 'Request',
+        ordered: 'Your order has been placed',
+        completed: 'Your order has completed',
+        declined: 'Your order has declined',
+        cancelled: 'Your order has cancelled',
+        accepted: 'Your order has accepted',
+    };
+
+
+    return (
+
+        <View
+            style={AppStyles.ProductInfoMainBg}
+        >
+            {item.orderStatus !== 'requested' && <View>
+
+
+
+                {/* Status details */}
+                <View style={AppStyles.StatusDetailBx}>
+
+                    <Text style={[AppStyles.OrderStatusText, { color: ColorType[item.orderStatus] }]}>{StatusText[item.orderStatus]}</Text>
+
+                    <Text style={AppStyles.OrderTime}>{moment(`${item.Date} ${item.time}`, "D MMMM YYYY h:mm A").fromNow()}</Text>
+
+                </View>
+
+
+
+                <View style={AppStyles.LineBg2} />
+
+
+
+                <View style={AppStyles.ViewContainerRow}>
+
+                    <View style={AppStyles.ViewContainerPrdDetail}>
+                        <Text
+                            style={AppStyles.ProdName}
+                            numberOfLines={2}
+                            ellipsizeMode="tail"
+                        >{`${item.traderName}`}
+                        </Text>
+
+                        <Text
+                            style={AppStyles.ProdName}
+                            numberOfLines={2}
+                            ellipsizeMode="tail"
+                        >{`${item.label} |  ${item.orderedQty}00 ${item.unit}`}
+                        </Text>
+
+                        {/* Location */}
+                        <View style={AppStyles.ProdInfoBg}>
+
+                            <LocationIcon height={14} width={14} color={Constants.AppSecondaryColor} />
+
+                            <Text style={AppStyles.ProdInfo}>{item.location}</Text>
+
+                        </View>
+
+                        {/* Time */}
+                        <View style={AppStyles.ProdInfoBg}>
+
+                            <TimeIcon height={14} width={14} color={Constants.AppSecondaryColor} />
+
+                            <Text style={AppStyles.ProdInfo}
+                                numberOfLines={1}
+                            >{`${item.Date} |  ${item.time}`}</Text>
+
+                        </View>
+
+                    </View>
+
+                    <View style={AppStyles.ViewContainerAmt}>
+
+                        {/* Amount */}
+                        <View style={AppStyles.AmountBg}>
+                            <Text style={AppStyles.AmountTxt}>₹ {`${item.price}/${item.unit}`}</Text>
+                        </View>
+
+                    </View>
+
+                </View>
+
+                {/* Line */}
+                <View style={AppStyles.LineBg} />
+
+
+                <View style={AppStyles.ViewContainerRow}>
+
+                    <Text style={AppStyles.Destxt}>Id - mh125</Text>
+
+                    <View style={AppStyles.DestBg}>
+                        <Text style={AppStyles.Destxt}>5 yrs</Text>
+                    </View>
+
+                    <AssuredPayIcon height={13} />
+
+                    <VerifiedIcon height={13} />
+
+                    {/* Rating */}
+                    <View style={AppStyles.ViewContainerRow}>
+                        <RatingIcon height={13} color={'#FF8B00'} />
+
+                        <Text style={AppStyles.Ratingtxt}>3.5</Text>
+                    </View>
+
+                </View>
+
+            </View>
+            }
+
+            {item.orderStatus === 'requested' &&
+                <View>
+
+
+                    {/* Status details */}
+                    <View style={AppStyles.StatusDetailBx}>
+
+                        <Text style={[AppStyles.OrderStatusText, { color: ColorType[item.orderStatus] }]}>{StatusText[item.orderStatus]}</Text>
+
+                        <Text style={AppStyles.OrderTime}>{'2 hrs ago'}</Text>
+
+                    </View>
+
+                    <View style={AppStyles.LineBg2} />
+
+                    <View style={AppStyles.ViewContainerRow}>
+
+                        <View style={AppStyles.ViewContainerPrdDetail}>
+
+                            <Text style={AppStyles.ReqText}
+                                numberOfLines={2}
+                                ellipsizeMode="tail"
+                            >
+                                Requested image for your order no. Id-mh125
+                            </Text>
+
+                            <Text
+                                style={AppStyles.ProdName}
+                                numberOfLines={2}
+                                ellipsizeMode="tail"
+                            >{`${item.label} |  ${item.orderedQty}00 ${item.unit}`}
+                            </Text>
+
+                        </View>
+
+
+                    </View>
+
+                </View>
+            }
+
+
+
+
+            {/* Status Box */}
+            <View
+                style={[AppStyles.StatusBoxBg, { backgroundColor: ColorType[item.orderStatus] },]}
+            />
+
+
+        </View>
+
+    );
+};
+
+export default CustNotificationItem;
+const { width, height } = Dimensions.get(Constants.ScreenType);
+
+const AppStyles = StyleSheet.create({
+
+    OrderStatusText:
+    {
+        fontFamily: 'DMSans-SemiBold',
+        fontSize: RFValue(13),
+        color: Colors.AppSecondaryColor,
+    },
+    OrderTime:
+    {
+        fontFamily: 'DMSans-Medium',
+        fontSize: RFValue(13),
+        color: '#636363',
+    },
+    StatusDetailBx:
+    {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    StatusBoxBg:
+    {
+        height: height * 0.04,
+        width: 13,
+        backgroundColor: '#FF8B00',
+        position: 'absolute',
+    },
+    LineBg:
+    {
+        width: '100%',
+        height: 1,
+        alignSelf: 'center',
+        backgroundColor: '#CBCBCB',
+        marginVertical: 10,
+    },
+    LineBg2:
+    {
+        width: '100%',
+        height: 1,
+        alignSelf: 'center',
+        backgroundColor: '#CBCBCB',
+        marginVertical: 2,
+    },
+    ViewContainerRow:
+    {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+
+    ViewContainerPrdDetail:
+    {
+        flex: 2,
+    },
+    ViewContainerAmt:
+    {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'flex-end',
+    },
+
+    ProductInfoMainBg:
+    {
+        backgroundColor: 'white',
+        paddingHorizontal: '5%',
+        paddingVertical: '3%',
+        marginTop: 10,
+
+    },
+    ReqText:
+    {
+        fontFamily: 'DMSans-SemiBold',
+        fontSize: RFValue(13),
+        color: Colors.AppSecondaryColor,
+    },
+
+    ProdName:
+    {
+        fontFamily: 'DMSans-Bold',
+        fontSize: RFValue(22),
+        color: Colors.AppSecondaryColor,
+        marginTop: 5,
+    },
+    ProdInfoBg:
+    {
+        flexDirection: 'row',
+        marginRight: '10%',
+        marginTop: 5,
+        alignItems: 'center',
+    },
+    ProdInfo:
+    {
+        fontFamily: 'DMSans-Medium',
+        fontSize: RFValue(13),
+        color: Colors.AppSecondaryColor,
+        marginLeft: 6,
+    },
+
+    AmountBg:
+    {
+        backgroundColor: Colors.AppSecondaryColor,
+        paddingVertical: 6,
+        borderRadius: 8,
+        alignItems: 'center',
+        paddingHorizontal: 15,
+    },
+    AmountTxt:
+    {
+        fontFamily: 'DMSans-Bold',
+        fontSize: RFValue(15),
+        color: 'white',
+    },
+    DestBg:
+    {
+        backgroundColor: '#ECECEC',
+        paddingVertical: 4,
+        borderRadius: 6,
+        alignItems: 'center',
+        paddingHorizontal: 10,
+    },
+    Destxt:
+    {
+        fontFamily: 'DMSans-SemiBold',
+        fontSize: RFValue(12),
+        color: Colors.AppSecondaryColor,
+    },
+    Ratingtxt:
+    {
+        fontFamily: 'DMSans-SemiBold',
+        fontSize: RFValue(12),
+        color: Colors.AppSecondaryColor,
+        marginLeft: 5,
+    },
+
+})
--- src/screens/Home/NotificationFragment.jsx
+++ src/screens/Home/NotificationFragment.jsx
@@ -1,14 +1,109 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Dimensions, FlatList, KeyboardAvoidingView, StyleSheet, Text, TextInput, View } from 'react-native';
+import React, { useState } from 'react';
+import Colors from '../../constants/Colors';
+import SearchIcon from '../../assets/images/ic_Search.svg';
+import { RFValue } from 'react-native-responsive-fontsize';
+import Constants from '../../constants/Constants';
+import CustNotificationItem from '../../components/CustomRenderItem/CustNotificationItem';
 
-const NotificationFragment = () =>{
+const NotificationFragment = () => {
+
+  const [search, setSearch] = useState('');
+
+
+  const ProdRecordList = [
+    { id: 1, label: 'Onion', location: 'Nampur , Nashik', Date: "3 April 2025", time: "2:30 pm", price: '10', unit: 'Kg', traderName: 'Rajesh Traders', orderTime: "3:00 pm", orderStatus: 'inprocess', orderedQty: '50' },
+    { id: 2, label: 'Onion', location: 'Nampur , Nashik', Date: "2 April 2025", time: "2:30 pm", price: '10', unit: 'Kg', traderName: 'Sharma Agro', orderTime: "3:15 pm", orderStatus: 'completed', orderedQty: '30' },
+    { id: 3, label: 'Onion', location: 'Nampur , Nashik', Date: "1 April 2025", time: "2:30 pm", price: '10', unit: 'Kg', traderName: 'Gupta Fresh', orderTime: "3:20 pm", orderStatus: 'requested', orderedQty: '20' },
+    { id: 4, label: 'Onion', location: 'Nampur , Nashik', Date: "3 April 2025", time: "2:30 pm", price: '10', unit: 'Kg', traderName: 'Verma Exports', orderTime: "3:25 pm", orderStatus: 'declined', orderedQty: '40' },
+    { id: 5, label: 'Onion', location: 'Nampur , Nashik', Date: "3 April 2023", time: "2:30 pm", price: '10', unit: 'Kg', traderName: 'Patel Traders', orderTime: "3:30 pm", orderStatus: 'cancelled', orderedQty: '10' },
+    { id: 6, label: 'Onion', location: 'Nampur , Nashik', Date: "3 April 2023", time: "2:30 pm", price: '10', unit: 'Kg', traderName: 'Singh Enterprises', orderTime: "3:35 pm", orderStatus: 'accepted', orderedQty: '60' },
+    { id: 7, label: 'Onion', location: 'Nampur , Nashik', Date: "3 April 2023", time: "2:30 pm", price: '10', unit: 'Kg', traderName: 'Yadav Fresh Foods', orderTime: "3:40 pm", orderStatus: 'ordered', orderedQty: '25' },
+    { id: 8, label: 'Onion', location: 'Nampur , Nashik', Date: "3 April 2023", time: "2:30 pm", price: '10', unit: 'Kg', traderName: 'Mehta & Sons', orderTime: "3:45 pm", orderStatus: 'completed', orderedQty: '35' },
+    { id: 9, label: 'Onion', location: 'Nampur , Nashik', Date: "3 April 2023", time: "2:30 pm", price: '10', unit: 'Kg', traderName: 'Jain Agro', orderTime: "3:50 pm", orderStatus: 'inprocess', orderedQty: '45' },
+    { id: 10, label: 'Onion', location: 'Nampur , Nashik', Date: "3 April 2023", time: "2:30 pm", price: '10', unit: 'Kg', traderName: 'Kumar Trading Co.', orderTime: "3:55 pm", orderStatus: 'requested', orderedQty: '55' },
+  ];
+
   return (
-    <View>
-      <Text>NotificationFragment</Text>
-    </View>
-  )
-}
+    <KeyboardAvoidingView style={AppStyles.MainContainer}>
+
+      <View style={AppStyles.flexOne} >
+
+        <View style={AppStyles.HeaderBg}>
+          {/* Search bar */}
+          <View style={AppStyles.SearchInputBg}>
+
+            <TextInput
+              style={AppStyles.SearchText}
+              placeholder="Search"
+              inputMode="search"
+              numberOfLines={1}
+              placeholderTextColor={Colors.InputBoxLayout}
+              onChangeText={setSearch}
+              value={search}
+            // returnKeyType=""
+
+            />
+
+            <SearchIcon width={25} height={25} color={'#E3E3E3'} />
+          </View>
+        </View>
+
+        <View style={AppStyles.flexOne}>
+          <FlatList
+            data={ProdRecordList}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={({ item }) => <CustNotificationItem item={item} />}
+            contentContainerStyle={{ paddingBottom: 20, }} // Ensures full scroll
+            showsVerticalScrollIndicator={false} // Optional: Hides scrollbar
+          />
+        </View>
+
+      </View>
+
+    </KeyboardAvoidingView>
+  );
+};
 
 export default NotificationFragment;
 
-const styles = StyleSheet.create({})
+const { width, height } = Dimensions.get(Constants.ScreenType);
+const TAB_BAR_HEIGHT = 70;
+const AppStyles = StyleSheet.create({
+
+  MainContainer:
+  {
+    flex: 1,
+    // backgroundColor: 'white',
+    paddingBottom: TAB_BAR_HEIGHT,
+  },
+  flexOne:
+  {
+    flex: 1,
+  },
+  HeaderBg:
+  {
+    backgroundColor: 'white',
+    // flexDirection: 'column',
+    // flex: 1,
+  },
+  SearchInputBg:
+  {
+    borderWidth: 1,
+    borderColor: Colors.InputBoxLayout,
+    borderRadius: 8,
+    paddingHorizontal: 20,
+    paddingVertical: 5,
+    backgroundColor: Colors.InputBoxBg,
+    marginHorizontal: '5%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical:15,
+  },
+  SearchText:
+  {
+    fontSize: RFValue(15),
+    flex: 1,
+    color: Colors.TextColor1,
+  },
+})
